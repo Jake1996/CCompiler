a0350a05fb9b49e6bb1f5cda6116d099c9686e3b
Jake1996 <jatinpm96@gmail.com>
2017-04-06 20:28:25 +0530

    debugging stage

--- src/Main/GCC.java
+++ src/Main/GCC.java
@@ -1,10 +1,30 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import Parser.Parser;
+
 public class GCC {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//System.out.println("working");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String program;
+//		StringBuilder sb = new StringBuilder();
+//		String s;
+//		try {
+//			while((s = br.readLine())!=null) {
+//				sb.append(s);
+//			}
+//			br.close();
+//		} catch (IOException e) {
+//			
+//		}
+//		program = sb.toString();
+		program = "int a = 5;\nint b = 6;\nint[5][7] c;\na = b*c[1][2];";
+		Parser parse = new Parser(program);
+		System.out.println(parse.output);
 	}
-
 }
--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,21 +1,13 @@
 package Parser;
 
 import LexicalAnalyzer.Lexer;
-import SymbolTable.Token;
 
 public class Parser {
 	public static Lexer lex;
-	public static void main(String args[]) {
-		Lexer l = new Lexer("for (int i = 1; i < N; i++) dp[i][0] = 1;for (int i = 1; i < N; i++) {for (int j = 1; j < N; j++) {if (i > j) {");
-		Token s;
-		while((s=l.getNextToken())!=null) {
-			System.out.println(s);
-		}
-	}
+	public String output;
 	public Parser(String program) {
 		lex = new Lexer(program);
-		String output = new stmts(null).code;
-		System.out.println(output);
+		output = new stmts(null).code;
 	}
 	
 }
